913a639112a38258872508bf7a75d54221c1b0b9
张兆华 <593245631@qq.com>
2019-03-26 13:15:03 +0800

    类和对象

--- class2/class2/源.cpp
+++ class2/class2/源.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 using namespace std;
 //class Date
@@ -36,31 +37,186 @@ using namespace std;
 //	return 0;
 //}
 //**************************************************************************************
-#include<assert.h>
-typedef int DataType;
-class SeqList
+//#include<assert.h>
+//typedef int DataType;
+//class SeqList
+//{
+//public:
+//	SeqList(int capacity = 10)
+//	{
+//		_pData = (DataType*)malloc(capacity * sizeof(DataType));
+//		assert(_pData);
+//
+//		_size = 0;
+//		_capacity = capacity;
+//	}
+//	~SeqList()
+//	{
+//		if (_pData)
+//		{
+//			free(_pData);
+//			_pData = NULL;
+//			_capacity = 0;
+//			_size = 0;
+//		}
+//	}
+//private:
+//	int* _pData;
+//	size_t _size;
+//	size_t _capacity;
+//};
+//*******************************************************************************************
+//class Date
+//{
+//public:
+//	Date()
+//	{
+//		_year = 2019;
+//		_month = 3;
+//		_day = 26;
+//	}
+//
+//	Date(int year = 2019, int month = 3, int day = 26)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	Date d1;
+//	return 0;
+//}
+//*******************************************************************************************
+////析构函数
+//class String
+//{
+//public:
+//	String(const char* str = "jack")
+//	{
+//		_str = (char*)malloc(strlen(str) + 1);
+//		strcpy(_str, str);
+//		cout << "String()" << endl;
+//	}
+//
+//	~String()
+//	{
+//		cout << "~String()" << endl;
+//		free(_str);
+//	}
+//private:
+//	char* _str;
+//};
+//class Person
+//{
+//private:
+//	String _name;
+//	int _age;
+//};
+//
+//int main()
+//{
+//
+//	Person p;
+//
+//	getchar();
+//	return 0;
+//}
+//********************************************************************************************
+////运算符重载
+//class Date
+//{
+//public:
+//	Date(int year = 2019, int month = 3, int day = 26)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	Date(const Date& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//	}
+//
+//	Date& operator=(const Date& d)
+//	{
+//		if (this != &d)
+//		{
+//			_year = d._year;
+//			_month = d._month;
+//			_day = d._day;
+//		}
+//		return *this;
+//	}
+//	bool operator==(const Date& d)
+//	{
+//		return _year == d._year&&_month == d._month&&_day == d._day;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	Date d1;
+//	Date d2(2020, 3, 28);
+//
+//	d1 = d2;
+//
+//	cout << (d1 == d2) << endl;
+//	getchar();
+//	return 0;
+//}
+//**********************************************************************************************
+//const成员
+class Date
 {
 public:
-	SeqList(int capacity = 10)
+	void Display()
 	{
-		_pData = (DataType*)malloc(capacity * sizeof(DataType));
-		assert(_pData);
-
-		_size = 0;
-		_capacity = capacity;
+		cout << "Display ()" << endl;
+		cout << "year:" << _year << endl;
+		cout << "month:" << _month << endl;
+		cout << "day:" << _day << endl << endl;
 	}
-	~SeqList()
+	void Display() const
 	{
-		if (_pData)
-		{
-			free(_pData);
-			_pData = NULL;
-			_capacity = 0;
-			_size = 0;
-		}
+		cout << "Display () const" << endl;
+		cout << "year:" << _year << endl;
+		cout << "month:" << _month << endl;
+		cout << "day:" << _day << endl << endl;
+	}
+	Date* operator&()
+	{
+		return this;
+	}
+	const Date* operator&() const
+	{
+		return this;
 	}
 private:
-	int* _pData;
-	size_t _size;
-	size_t _capacity;
+	int _year; // 年
+	int _month; // 月
+	int _day; // 日
 };
+void Test()
+{
+	Date d1;
+	d1.Display();
+
+	const Date d2;
+	d2.Display();
+
+	Date& d3 = d1;
+
+	d3.Display();
+}int main(){	Test();	getchar();	return 0;}
