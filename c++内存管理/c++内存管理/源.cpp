8d1665ff5a8a456706b021cf715300dcb0535847
张兆华 <593245631@qq.com>
2019-03-27 10:19:42 +0800

    内存管理

--- c++内存管理/c++内存管理/源.cpp
+++ c++内存管理/c++内存管理/源.cpp
@@ -48,30 +48,71 @@ using namespace std;
 //	delete ptr2;
 //	delete[] ptr3;
 //}
-
-class Test
-{
-public:
-	Test()
-		: _data(0)
-	{
-		cout << "Test():" << this << endl;
-	}
-	~Test()
-	{
-		cout << "~Test():" << this << endl;
-	}
-
-private:
-	int _data;
-};
-int main()
-{
-	Test* t = new Test;
-	delete t;
-	cout << "--------------" << endl;
-	Test* t1 = (Test*)malloc(sizeof(Test));
-	free(t1);
-	getchar();
-	return 0;
-}
+//
+//class Test
+//{
+//public:
+//	Test()
+//		: _data(0)
+//	{
+//		cout << "Test():" << this << endl;
+//	}
+//	~Test()
+//	{
+//		cout << "~Test():" << this << endl;
+//	}
+//
+//private:
+//	int _data;
+//};
+//int main()
+//{
+//	Test* t = new Test;
+//	delete t;
+//	cout << "--------------" << endl;
+//	Test* t1 = (Test*)malloc(sizeof(Test));
+//	free(t1);
+//	getchar();
+//	return 0;
+//}
+//operator new / operator delete
+//struct ListNode
+//{
+//	ListNode* _next;
+//	ListNode* _prev;
+//	int _data;
+//	void* operator new(size_t n)
+//	{
+//		void* p = nullptr;
+//		p = allocator<ListNode>().allocate(1);
+//		cout << "memory pool allocate" << endl;
+//		return p;
+//	}
+//	void operator delete(void* p)
+//	{
+//		allocator<ListNode>().deallocate((ListNode*)p, 1);
+//		cout << "memory pool deallocate" << endl;
+//	}
+//};//class List//{//private://	ListNode* _head;//public://	List()//	{//		_head = new ListNode;//		_head->_next = _head;//		_head->_prev = _head;//	}//	~List()//	{//		cout << 111 << endl;//		ListNode* cur = _head->_next;//		while (cur != _head)//		{//			_head->_next = cur->_next;//			delete cur;//			cur = _head->_next;//		}//		delete _head;//		_head = nullptr;//	}//};//int main()//{//	List l;//	List* p = new List;//	delete p;//	getchar();//	return 0;//}//定位new表达式//class Test
+//{
+//public:
+//	Test()
+//		: _data(0)
+//	{
+//		cout << "Test():" << this << endl;
+//	}
+//	~Test()
+//	{
+//		cout << "~Test():" << this << endl;
+//	}
+//
+//private:
+//	int _data;
+//};
+//void Test1()
+//{
+//	// pt现在指向的只不过是与Test对象相同大小的一段空间，还不能算是一个对象，因为构造函数没有执行
+//	Test* pt = (Test*)malloc(sizeof(Test));
+//
+//	new(pt)Test; // 注意：如果Test类的构造函数有参数时，此处需要传参
+//}
