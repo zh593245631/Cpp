4bfb42c67ca958e625410f2045bd9e55000bf9b6
张兆华 <593245631@qq.com>
2019-05-25 23:51:46 +0800

    二叉搜索树模

--- 二叉搜索树的实现/BSTree.hpp
+++ 二叉搜索树的实现/BSTree.hpp
@@ -70,6 +70,79 @@ public:
 			parent->_pRight = newNode;
 		return true;
 	}
+	//É¾³ý
+	bool Erase(const T& val)
+	{
+		if (_root  == nullptr)
+			return false;
+
+		pNode cur = _root;
+		pNode parent = nullptr;
+
+		while (cur)
+		{
+			if (cur->_val == val) {
+				break;
+			}
+			else if (cur->_val > val) {
+				parent = cur;
+				cur = cur->_pLeft;
+			}
+			else {
+				parent = cur;
+				cur = cur->_pRight;
+			}
+		}
+		//Ã»ÕÒµ½
+		if (cur == nullptr)
+			return false;
+
+		if (cur->_pLeft == nullptr) {
+			if (cur == _root) {
+				_root = cur->_pRight;
+			}
+			else {
+				if (parent->_val > val) {
+					parent->_pLeft = cur->_pRight;
+				}
+				else {
+					parent->_pRight = cur->_pRight;
+				}
+			}
+			delete cur;
+			cur = nullptr;
+		}
+		else if (cur->_pRight == nullptr) {
+			if (cur == _root) {
+				_root = cur->_pLeft;
+			}
+			else {
+				if (parent->_val > val) {
+					parent->_pLeft = cur->_pLeft;
+				}
+				else {
+					parent->_pRight = cur->_pLeft;
+				}
+			}
+			delete cur;
+			cur = nullptr;
+		}
+		else {
+			pNode next = cur->_pLeft;
+			parent = cur;
+			while (next->_pRight) {
+				parent = next;
+				next = next->_pRight;
+			}
+			cur->_val = next->_val;
+			if (next->_pLeft == nullptr)
+				parent->_pRight = nullptr;
+			parent->_pRight = next->_pLeft;
+			delete next;
+			next= nullptr;
+		}
+		return true;
+	}
 	void print()
 	{
 		_InOrder(_root);
--- 二叉搜索树的实现/源.cpp
+++ 二叉搜索树的实现/源.cpp
@@ -5,17 +5,27 @@ int main()
 	BST<int> b;
 
 	b.insert(6);
-	b.print();
+	//b.print();
 	b.insert(1);
-	b.print();
+	//b.print();
 	b.insert(2);
-	b.print();
+	//b.print();
 	b.insert(5);
-	b.print();
+	//b.print();
 	b.insert(3);
-	b.print();
+	//b.print();
 	b.insert(4);
 	b.print();
+
+	b.Erase(2);
+	b.print();
+	b.Erase(3);
+	b.print();
+	b.Erase(6);
+	b.print();
+	b.Erase(1);
+	b.print();
+
 
 
 
