e83f8838045781ad2f708a3927a4940d6dc1b1c5
张兆华 <593245631@qq.com>
2019-06-02 22:22:02 +0800

    内存管理复习 只在堆上创建对象  只在栈上创建对象

--- c++内存管理/c++内存管理/源.cpp
+++ c++内存管理/c++内存管理/源.cpp
@@ -93,7 +93,42 @@ using namespace std;
 //		allocator<ListNode>().deallocate((ListNode*)p, 1);
 //		cout << "memory pool deallocate" << endl;
 //	}
-//};//class List//{//private://	ListNode* _head;//public://	List()//	{//		_head = new ListNode;//		_head->_next = _head;//		_head->_prev = _head;//	}//	~List()//	{//		cout << 111 << endl;//		ListNode* cur = _head->_next;//		while (cur != _head)//		{//			_head->_next = cur->_next;//			delete cur;//			cur = _head->_next;//		}//		delete _head;//		_head = nullptr;//	}//};//int main()//{//	List l;//	List* p = new List;//	delete p;//	getchar();//	return 0;//}//定位new表达式//class Test
+//};
+//class List
+//{
+//private:
+//	ListNode* _head;
+//public:
+//	List()
+//	{
+//		_head = new ListNode;
+//		_head->_next = _head;
+//		_head->_prev = _head;
+//	}
+//	~List()
+//	{
+//		cout << 111 << endl;
+//		ListNode* cur = _head->_next;
+//		while (cur != _head)
+//		{
+//			_head->_next = cur->_next;
+//			delete cur;
+//			cur = _head->_next;
+//		}
+//		delete _head;
+//		_head = nullptr;
+//	}
+//};
+//int main()
+//{
+//	List l;
+//	List* p = new List;
+//	delete p;
+//	getchar();
+//	return 0;
+//}
+//定位new表达式
+//class Test
 //{
 //public:
 //	Test()
@@ -115,4 +150,90 @@ using namespace std;
 //	Test* pt = (Test*)malloc(sizeof(Test));
 //
 //	new(pt)Test; // 注意：如果Test类的构造函数有参数时，此处需要传参
-//}
+//}
+
+
+int globalVar = 1; //数据区
+static int staticGlobalVar = 1;//数据区
+void Test()
+{
+	static int staticVar = 1; //数据区
+	int localVar = 1; //栈
+
+	int num1[10] = { 1, 2, 3, 4 };//栈
+	char char2[] = "abcd";   //内容代码段
+	char* pChar3 = "abcd";		// 。。
+	int* ptr1    = (int*)malloc(sizeof(int) * 4); //申请的空间在堆上
+	int* ptr2    = (int*)calloc(4, sizeof(int));
+	int* ptr3    = (int*)realloc(ptr2, sizeof(int) * 4);
+	
+	
+	cout << sizeof(ptr1) << endl;
+	cout << sizeof(ptr2) << endl;
+	cout << sizeof(ptr3) << endl;
+
+	free(ptr1);
+	free(ptr3);
+}void Test2()
+{
+	// 动态申请一个int类型的空间
+	int* ptr4 = new int;
+
+	// 动态申请一个int类型的空间并初始化为10
+	int* ptr5 = new int(10);
+
+	// 动态申请10个int类型的空间
+	int* ptr6 = new int[3];
+
+	delete ptr4;
+	delete ptr5;
+	delete[] ptr6;
+}
+class TT
+{
+public:
+	TT()
+		: _data(0)
+	{
+		cout << "TT():" << this << endl;
+	}
+
+	~TT()
+	{
+		cout << "~TT():" << this << endl;
+	}
+
+private:
+	int _data;
+};
+
+void Test3()
+{
+	// 申请单个Test类型的空间
+	TT* p1 = (TT*)malloc(sizeof(TT));
+	free(p1);
+
+	// 申请10个TT类型的空间
+	TT* p2 = (TT*)malloc(sizeof(TT) * 10);
+	free(p2);
+}
+void Test4()
+{
+	// 申请单个TT类型的对象
+	TT* p1 = new TT;
+	delete p1;
+
+	// 申请10个TT类型的对象
+	TT* p2 = new TT[10];
+	delete[] p2;
+}
+
+int main()
+{
+	cout << "Test3: " << endl;
+	Test3();
+
+	cout << "Test4: " << endl;
+	Test4();
+	return 0;
+}
