c0bc1aa7d86fe1ce5528e0b123577138a18cd44f
张兆华 <593245631@qq.com>
2019-06-11 18:01:48 +0800

    哈希表 闭散列 开散列

---
+++ HashTable/hash.h
@@ -0,0 +1,172 @@
+#pragma once
+//闭散列
+#include<vector>
+#include<iostream>
+using namespace std;
+
+namespace CLOSE
+{
+	enum STATA
+	{
+		EMPTY,
+		DELETE,
+		EXIST
+	};
+
+	template<class K, class V>
+	struct HashNode
+	{
+		pair<K, V> _kv;
+		STATA _stata = EMPTY;
+	};
+
+	template<class K, class V>
+	class HashTable
+	{
+	public:
+		typedef HashNode<K, V> Node;
+		typedef Node* pNode;
+	public:
+		HashTable(size_t size = 5)
+		{
+			_table.resize(size);
+			_size = 0;
+		}
+		bool insert(const pair<K, V>& kv)
+		{
+			CheckCapcity();
+			//位置映射
+			size_t index = kv.first % _table.size();
+			//找个地方插
+			while (_table[index]._stata != EMPTY)
+			{
+				//key不能重复
+				if (_table[index]._kv.first == kv.first)
+					return false;
+				++index;
+				if (index == _table.size())
+					index = 0;
+			}
+			_table[index]._kv = kv;
+			_table[index]._stata = EXIST;
+			++_size;
+			return true;
+		}
+		pNode Find(const K & k)
+		{
+			size_t index = k % _table.size();
+			while (_table[index]._stata != EMPTY)
+			{
+				if (_table[index]._stata == EXIST && _table[index]._kv.first == k)
+					return &_table[index];
+				++index;
+				if (index == _table.size())
+					index = 0;
+			}
+			return nullptr;
+		}
+		bool Erase(const K & k)
+		{
+			pNode ret = Find(k);
+			if (ret)
+			{
+				ret->_stata = DELETE;
+				--_size;
+				return true;
+			}
+			return false;
+		}
+	private:
+		void CheckCapcity()
+		{
+			if (_table.size() == 0
+				|| _size * 10 / _table.size() >= 7)
+			{
+				size_t newS = _table.size() == 0 ? 10 : 2 * _table.size();
+				HashTable newT(newS);
+				for (int i = 0; i < _table.size(); ++i)
+				{
+					if (_table[i]._stata == EXIST)
+					{
+						newT.insert(_table[i]._kv);
+					}
+				}
+				_table.swap(newT._table);
+			}
+		}
+	private:
+		vector<Node> _table;
+		size_t _size;
+	};
+}
+
+
+//开散列
+template<class V>
+struct HashNode
+{
+	HashNode<V>* _next;
+	V _data;
+
+	HashNode(const V& data)
+		:_next(nullptr)
+		,_data(data)
+	{}
+};
+
+template<class K, class V,class KeyOfValue>
+class HashTable
+{
+public:
+	typedef HashNode<V> Node;
+	typedef Node* pNode;
+public:
+	bool insert(const V& data)
+	{
+		CheckCapacity();
+		KeyOfValue kov;
+		size_t index = kov(data) % _table.size();
+		pNode cur = _table[index];
+		while (cur)
+		{
+			if(kov(cur->_data) == kov(data)
+				return false;
+			cur = cur->_next;
+		}
+		cur = new Node(data);
+		//头插
+		cur->_next = _table[index];
+		_table[index] = cur;
+		++_size;
+		return true;
+	}
+private:
+	void CheckCapacity()
+	{
+		if (_size == _table.size())
+		{
+			size_t newS = _table.size() == 0 ? 10 : _table.size() * 2;
+			vector<pNode> newV;
+			newV.resize(newS);
+			KeyOfValue kov;
+			//遍历旧表
+			for (size_t i = 0; i < _table.size(); ++i)
+			{
+				pNode cur = _table[i];
+				while (cur)
+				{
+					pNode next = cur->_next;
+					size_t index = kov(cur->_data) % newS;
+					cur->_next = newV[i];
+					newV[i] = cur;
+					cur = next;
+				}
+				_table[i] = nullptr;
+			}
+			_table.swap(newV);
+		}
+	}
+private:
+	vector<pNode> _table;
+	size_t size = 0;
+};
---
+++ HashTable/源.cpp
@@ -0,0 +1,25 @@
+#include"hash.h"
+
+using namespace std;
+
+//void test()
+//{
+//	using namespace CLOSE;
+//	HashTable<int, int> h;
+//	h.insert(pair<int, int>(1, 2));
+//	h.insert(pair<int, int>(1, 2));
+//	h.insert(pair<int, int>(2, 2));
+//	h.insert(pair<int, int>(3, 2));
+//	h.insert(pair<int, int>(4, 2));
+//	h.insert(pair<int, int>(5, 2));
+//	h.insert(pair<int, int>(6, 2));
+//	h.insert(pair<int, int>(14, 2));
+//	h.Erase(4);
+//	h.insert(pair<int, int>(14, 2));
+//
+//}
+int main()
+{
+	test();
+	return 0;
+}
